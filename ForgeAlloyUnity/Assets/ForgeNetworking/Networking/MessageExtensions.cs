b6bff46dbadd69b7929f5b69a2ff30ff8c0dbd57
Brent Farris <brentfarris@outlook.com>
2021-01-04 11:22:41 -0600

    Updating with what is on the alloy branch of FNR

---
+++ ForgeAlloyUnity/Assets/ForgeNetworking/Networking/MessageExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Forge.Networking.Messaging;
+
+namespace Forge.Networking
+{
+	public static class MessageExtensions
+	{
+		public static void ForwardToOtherClients(this INetworkMediator netMediator,
+			EndPoint sender, IMessage message)
+		{
+			if (!netMediator.IsServer)
+			{
+				throw new SocketNotServerException("Tried to forward a message" +
+					"to connected clients, but the current socket is not a server");
+			}
+			var itr = netMediator.PlayerRepository.GetEnumerator();
+			while (itr.MoveNext())
+			{
+				if (itr.Current != null && itr.Current.EndPoint != sender)
+					netMediator.SendReliableMessage(message, itr.Current.EndPoint);
+			}
+		}
+
+		public static void RunMessageLocally(
+			this INetworkMediator netMediator, IMessage message)
+		{
+			var interpreter = message.Interpreter;
+			if (ShouldInterpret(netMediator, interpreter))
+			{
+				interpreter.Interpret(netMediator,
+					netMediator.SocketFacade.ManagedSocket.EndPoint, message);
+			}
+		}
+
+		private static bool ShouldInterpret(
+			INetworkMediator mediator, IMessageInterpreter interpreter)
+		{
+			return (mediator.IsClient && interpreter.ValidOnClient)
+				|| (mediator.IsServer && interpreter.ValidOnServer);
+		}
+	}
+}
---
+++ ForgeAlloyUnity/Assets/ForgeNetworking/Networking/Messaging/Paging/IPaginatedMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Forge.Serialization;
+
+namespace Forge.Networking.Messaging.Paging
+{
+	public interface IPaginatedMessage
+	{
+		BMSByte Buffer { get; set; }
+		int TotalSize { get; }
+		List<IMessagePage> Pages { get; }
+	}
+}
